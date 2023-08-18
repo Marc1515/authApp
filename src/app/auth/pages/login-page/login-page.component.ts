cea29c71047802a5b42d9aab12c36d33ac74ab60
Marc España Quinquilla <meq.1515@gmail.com>
2023-08-18 17:52:08 +0200

    Inyectamos el servicio Router de Angular para hacer la navegación al dashboard cuando tenga éxito la autenticación del login.

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,8 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 import Swal from 'sweetalert2'
+
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-login-page',
@@ -11,12 +14,13 @@ import Swal from 'sweetalert2'
 })
 export class LoginPageComponent {
 
-  private fb = inject( FormBuilder );
+  private fb          = inject( FormBuilder );
   private authService = inject( AuthService );
+  private router      = inject( Router )
 
   public myForm: FormGroup = this.fb.group({
     email: ['meq.1515@gmail.com', [Validators.required, Validators.email]],
-    password: ['1234567', [Validators.required, Validators.minLength(6)]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
   })
 
 
@@ -26,7 +30,7 @@ export class LoginPageComponent {
 
     this.authService.login( email, password )
       .subscribe({
-        next: () => console.log('Todo bien'),
+        next: () => this.router.navigateByUrl('/dashboard'),
         error: (message) => {
           Swal.fire('Error', message, 'error')
         }
