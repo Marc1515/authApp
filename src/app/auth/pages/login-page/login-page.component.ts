3d51c7d712419e1a073320251553c6b2a4c385db
Marc España Quinquilla <meq.1515@gmail.com>
2023-08-18 17:12:12 +0200

    Importamos la librería SweetAlert2 y con su método fire eligiendo como tercer argumento adecuado para esta situación que sería 'error', el mensage como segundo argumento que recibimos desde nuestro Backend y el primer argumento Sería el título de la alerta.

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
+import Swal from 'sweetalert2'
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -13,8 +15,8 @@ export class LoginPageComponent {
   private authService = inject( AuthService );
 
   public myForm: FormGroup = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    email: ['meq.1515@gmail.com', [Validators.required, Validators.email]],
+    password: ['1234567', [Validators.required, Validators.minLength(6)]],
   })
 
 
@@ -23,8 +25,11 @@ export class LoginPageComponent {
     const { email, password } = this.myForm.value;
 
     this.authService.login( email, password )
-      .subscribe( success => {
-        console.log( {success} )
+      .subscribe({
+        next: () => console.log('Todo bien'),
+        error: (message) => {
+          Swal.fire('Error', message, 'error')
+        }
       })
 
   }
