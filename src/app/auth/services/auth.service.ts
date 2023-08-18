2d354007b1b913a353393a2cbfe98568ff7cb7dc
Marc España Quinquilla <meq.1515@gmail.com>
2023-08-18 19:02:26 +0200

    Creo un Observable checkAuthStatus para poder comprobar la autenticación del usuario donde compruebo el token y si está autenticado o no.

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { enviroments } from 'src/enviroments/enviroments';
 import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 
-import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { AuthStatus, CheckTokenResponse, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -44,6 +44,33 @@ export class AuthService {
 
         )
       )
+
+  }
+
+  checkAuthStatus(): Observable<boolean> {
+
+    const url   = `${ this.baseUrl }/auth/check-token`;
+    const token = localStorage.getItem('token');
+
+    if (!token) return of (false);
+
+    const headers = new HttpHeaders()
+      .set('Authorization', `Bearer ${ token }`);
+
+      return this.http.get<CheckTokenResponse>(url, { headers })
+        .pipe(
+          map( ({ token, user }) => {
+            this._currentUser.set( user );
+            this._authStatus.set( AuthStatus.authenticated );
+            localStorage.setItem('token', token);
+
+            return true;
+          }),
+          catchError(() => {
+            this._authStatus.set( AuthStatus.notAuthenticated );
+            return of(false)
+          })
+        )
 
   }
 
